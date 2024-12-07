5e1481f2e04e8229f1680668c76cf8e0075ae81f
Nandor <unlucky.hun5@gmail.com>
2024-12-07 16:36:12 +0100

    KisNagy Task done PDF file uploading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    kisnagy();
+}
+
+fn kisnagy() {
+    println!("First number: ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let number1: i32 = input.trim().parse().expect("Please type in a number");
+
+    println!("Second number: ");
+    input.clear();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let number2: i32 = input.trim().parse().expect("Please type in a number");
+
+    let (bigger, smaller) = if number1 > number2 {
+        (number1, number2)
+    } else {
+        (number2, number1)
+    };
+
+    let remain = bigger % smaller;
+    let outcome = bigger / smaller;
+
+    println!(
+        "The larger number contains the smaller number {} times, with a remainder of {}.",
+        outcome, remain
+    );
 }
